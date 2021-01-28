b3e5b3f3d0ae7c95fcc5a64022dd596cad7cf0bc
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-01-28 01:36:58 -0500

    feat: add some rough query param stuff

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,11 +2,31 @@ import 'canvas-toBlob';
 import 'preact';
 import { render } from 'preact';
 
+function parseInput(input: string) {
+	try {
+		return JSON.parse(atob(input));
+	} catch {
+		return undefined;
+	}
+}
+function generateOutput(output: unknown) {
+	return btoa(JSON.stringify(output));
+}
+
 function App() {
+	const input = window.location.search.replace('?', '');
+	const inputObj = parseInput(input) || { test: { foo: 'bar' } };
 	return (
 		<main>
 			<h1>TODO: title</h1>
-			<section>TODO: the whole game</section>
+			<section>
+				TODO: the whole game
+				<br />
+				input:
+				<pre style={{ textAlign: 'initial' }}>{JSON.stringify(inputObj, undefined, '\t')}</pre>
+				output:
+				<pre>{generateOutput(inputObj)}</pre>
+			</section>
 		</main>
 	);
 }
