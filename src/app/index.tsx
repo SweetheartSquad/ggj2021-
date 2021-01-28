db803e9c38925a560e51e88a05cc38b0263200bc
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-01-28 00:48:46 -0500

    Initial commit
    	modified:   .github/workflows/main.yml

---
+++ src/app/index.tsx
@@ -0,0 +1,14 @@
+import 'canvas-toBlob';
+import 'preact';
+import { render } from 'preact';
+
+function App() {
+	return (
+		<main>
+			<h1>TODO: title</h1>
+			<section>TODO: the whole game</section>
+		</main>
+	);
+}
+
+render(<App />, document.body);
