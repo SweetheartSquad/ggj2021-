03fa60f9f7c803d158f51ec327cbcf10dc5badcc
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-01-30 21:58:13 -0500

    reduce overdraw

--- src/app/Text.tsx
+++ src/app/Text.tsx
@@ -1,12 +1,14 @@
 import { useMemo } from 'preact/hooks';
 import { JSXInternal } from 'preact/src/jsx';
-import { mapHeight, mapSpacing, mapWidth } from './config';
+import { mapSpacing } from './config';
 import { TextPiece } from './TextPiece';
 import { useGridStyle } from './utils';
 
 export function Text({ x, y, children, ...props }: JSXInternal.HTMLAttributes<HTMLSpanElement> & { x: number; y: number; children: string }) {
 	const gridStyle = useGridStyle(x, y);
-	const subgridStyle = useMemo(() => ({ ...gridStyle, gridTemplateColumns: `repeat(${mapWidth}, ${mapSpacing}rem)`, gridTemplateRows: `repeat(${mapHeight}, ${mapSpacing}rem)` }), [gridStyle]);
+	const w = useMemo(() => children.split('\n').reduce((max,i) => Math.max(max, i.length), 0), [children]);
+	const h = useMemo(() => children.split('\n').length, [children]);
+	const subgridStyle = useMemo(() => ({ ...gridStyle, gridTemplateColumns: `repeat(${w}, ${mapSpacing}rem)`, gridTemplateRows: `repeat(${h}, ${mapSpacing}rem)` }), [gridStyle]);
 	return (
 		<span className="subgrid" style={subgridStyle} {...props}>
 			{children.split('\n').map((row, y) =>
