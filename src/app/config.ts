e8db12f5ba0ee3f0690eda5916e653ea5a4c4b1d
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-01-31 00:35:58 -0500

    fix: better distribution of star symbols

--- src/app/config.ts
+++ src/app/config.ts
@@ -4,7 +4,7 @@ export const mapHeight = 48;
 export const mapMinStars = 30;
 export const mapMaxStars = 50;
 export const mapSpacing = 0.9;
-export const mapStars = ['*', '*', 'o', 'O', '.', '+'];
+export const mapStars = ['*', '*', '*', 'o', 'O', '.', '.', '.', '+', '+'];
 export const traceryConstellations = {
 	group: ['belt', 'arm', 'leg', 'heart', 'collection', 'prince', 'princess', 'bastard', 'ensorcellment', 'enchantment', 'convalescence'],
 	grouping: ['stars', 'patience', 'death', 'possession', 'temperament', 'perspective', 'ambition', 'leadership'],
