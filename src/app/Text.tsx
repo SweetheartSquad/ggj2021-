71448cc9ee837a55fe57940254037d2b091ee308
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-01-30 22:11:16 -0500

    use a span for text if not interactive

--- src/app/Text.tsx
+++ src/app/Text.tsx
@@ -6,11 +6,12 @@ import { useGridStyle } from './utils';
 
 export function Text({ x, y, children, ...props }: JSXInternal.HTMLAttributes<HTMLLabelElement> & { x: number; y: number; children: string }) {
 	const gridStyle = useGridStyle(x, y);
-	const w = useMemo(() => children.split('\n').reduce((max,i) => Math.max(max, i.length), 0), [children]);
+	const w = useMemo(() => children.split('\n').reduce((max, i) => Math.max(max, i.length), 0), [children]);
 	const h = useMemo(() => children.split('\n').length, [children]);
+	const Tag = props.htmlFor ? 'label' : 'span';
 	const subgridStyle = useMemo(() => ({ ...gridStyle, gridTemplateColumns: `repeat(${w}, ${mapSpacing}rem)`, gridTemplateRows: `repeat(${h}, ${mapSpacing}rem)` }), [gridStyle]);
 	return (
-		<label className="subgrid" style={subgridStyle} {...props}>
+		<Tag className="subgrid" style={subgridStyle} {...props}>
 			{children.split('\n').map((row, y) =>
 				row.split('').map((character, x) => (
 					<TextPiece key={`${y}-${x}`} x={x} y={y}>
@@ -18,6 +19,6 @@ export function Text({ x, y, children, ...props }: JSXInternal.HTMLAttributes<HT
 					</TextPiece>
 				))
 			)}
-		</label>
+		</Tag>
 	);
 }
