0a3395aa0911ce23d05b79a02996154143807eac
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2021-01-30 21:58:23 -0500

    make text a label
    will probably use for buttons mostly

--- src/app/Text.tsx
+++ src/app/Text.tsx
@@ -4,13 +4,13 @@ import { mapSpacing } from './config';
 import { TextPiece } from './TextPiece';
 import { useGridStyle } from './utils';
 
-export function Text({ x, y, children, ...props }: JSXInternal.HTMLAttributes<HTMLSpanElement> & { x: number; y: number; children: string }) {
+export function Text({ x, y, children, ...props }: JSXInternal.HTMLAttributes<HTMLLabelElement> & { x: number; y: number; children: string }) {
 	const gridStyle = useGridStyle(x, y);
 	const w = useMemo(() => children.split('\n').reduce((max,i) => Math.max(max, i.length), 0), [children]);
 	const h = useMemo(() => children.split('\n').length, [children]);
 	const subgridStyle = useMemo(() => ({ ...gridStyle, gridTemplateColumns: `repeat(${w}, ${mapSpacing}rem)`, gridTemplateRows: `repeat(${h}, ${mapSpacing}rem)` }), [gridStyle]);
 	return (
-		<span className="subgrid" style={subgridStyle} {...props}>
+		<label className="subgrid" style={subgridStyle} {...props}>
 			{children.split('\n').map((row, y) =>
 				row.split('').map((character, x) => (
 					<TextPiece key={`${y}-${x}`} x={x} y={y}>
@@ -18,6 +18,6 @@ export function Text({ x, y, children, ...props }: JSXInternal.HTMLAttributes<HT
 					</TextPiece>
 				))
 			)}
-		</span>
+		</label>
 	);
 }
